57193486097536628c6fbe03983227bd594e9c8c
Zach Arnold <zarnold@hamilton.edu>
2014-09-11 16:48:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HAPApp::Application.config.secret_token = '7da40f3b8cb0ade4fc405f5ea690228f04583c93d06d19502ba7832e945f82efbec6544a08415e7dcc14655484d8148514af9b70169f10274d545b3f3058b1c9'
