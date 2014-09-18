e42e20227c7c5566777c9447ad5e23e7000826e1
rlfriedm <rlfriedm@hamilton.edu>
2014-09-18 15:41:05 -0400

    added basic trail page

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -6,4 +6,9 @@ class TrailsController < ApplicationController
   	Trail.create params[:trail]
 	redirect_to action: 'index'
   end
+  def trail
+  	id = params[:id]
+  	@trail = Trail.find(id)
+  end
+
 end
