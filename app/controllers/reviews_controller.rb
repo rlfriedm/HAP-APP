3474a4b29981cdb27e51b1da2ab960b5c441cb10
rlfriedm <rlfriedm@hamilton.edu>
2014-10-18 23:51:26 -0400

    Multiple photo upload per review :)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,15 @@ class ReviewsController < ApplicationController
 	def create
 		id = params[:review][:trail_id]
 		@trail = Trail.find(id)
-		params[:review][:photo] = Photo.new(:image => params[:review][:photo])
+
+		(params[:review][:photos]).each_with_index do |photo, i|
+			params[:review][:photos][i] = Photo.new(:image => photo)
+		end
+
+		photos = params[:review][:photos]
+
 		@review = @trail.reviews.build(params[:review])
+	
 		if @review.save!
 			redirect_to :back
 		end
@@ -13,7 +20,7 @@ class ReviewsController < ApplicationController
 	def deleteReview
   		reviews = Trail.find(params[:trail_id]).reviews
   		review = reviews.find(params[:review_id])
-  		review.photo.destroy
+  		review.photos.destroy
   		review.destroy
   		redirect_to :back
   	end
