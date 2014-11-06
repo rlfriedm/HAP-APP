fde68b31f026bc963cb432891dec6b982ea0f371
Zachary Arnold <zarnold@gemini.cs.hamilton.edu>
2014-11-06 13:46:33 -0500

    fixed database issues with trail and made styling changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141031191957) do
+ActiveRecord::Schema.define(:version => 20141106180600) do
 
   create_table "comments", :force => true do |t|
     t.text     "text"
