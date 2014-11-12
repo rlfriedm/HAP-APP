336e32554955302829e908b0183176d5883d38b0
Gretchen Walker <gwalker1228@gmail.com>
2014-11-12 13:03:52 -0500

    No real changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20141106180600) do
 
   create_table "comments", :force => true do |t|
@@ -63,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20141106180600) do
     t.integer  "rating",      :limit => 5
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
